103d693e4b03cdcb87fa8ea28b0226e79375fc1a
Shankarn9060 <97588329+Shankarn9060@users.noreply.github.com>
2022-02-22 20:10:02 +0530

    #include<stdio.h> #include<math.h> int main() {    int n;    scanf("%d", &n);
      for(int i = n; i >= 1; i--)
    {
      for(int j = n; j > i; j--)
        printf("%d ", j);
      for(int j = 1; j <= 2 * i - 1; j++)
        printf("%d ", i);
      for(int j = i + 1; j <= n; j++)
        printf("%d ", j);
        printf("
    ");
    }
    
     for(int i = 1; i < n; i++)
    {
     for(int j = n; j > i; j--)
       printf("%d ", j);
     for(int j = 1; j <= 2 * i - 1; j++)
       printf("%d ", i + 1);
     for(int j = i + 1; j <= n; j++)
       printf("%d ", j);
       printf("
    ");
    }
    
    return 0;
    }

---
+++ Printing_Pattern_using_Loops.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int n;
+   scanf("%d", &n);
+
+  for(int i = n; i >= 1; i--)
+{
+  for(int j = n; j > i; j--)
+    printf("%d ", j);
+  for(int j = 1; j <= 2 * i - 1; j++)
+    printf("%d ", i);
+  for(int j = i + 1; j <= n; j++)
+    printf("%d ", j);
+    printf("
+");
+}
+
+ for(int i = 1; i < n; i++)
+{
+ for(int j = n; j > i; j--)
+   printf("%d ", j);
+ for(int j = 1; j <= 2 * i - 1; j++)
+   printf("%d ", i + 1);
+ for(int j = i + 1; j <= n; j++)
+   printf("%d ", j);
+   printf("
+");
+}
+
+return 0;
+}
