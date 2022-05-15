471568b7a179d145a5642536e3081ad4761a3010
Shankarn9060 <97588329+Shankarn9060@users.noreply.github.com>
2022-05-15 12:26:27 +0530

    #include<stdio.h> int main() { 	int n,v,k; 	scanf("%d",&n); 	for(v=n;v>=1;v--) 	{ 		for(k=1;k<=n;k++) 		{ 			if(k+v==n+1||k-v==0) 			{ 				printf("%d ",v); 			} 			else 			{ 				printf(" "); 			} 		} 		printf(" "); 	} 	return 0; }

--- Print_the_following_pattern.c
+++ Print_the_following_pattern.c
@@ -1,16 +1,23 @@
-#include <stdio.h>
-int main() 
- {
-  int rows, i, j;
-  scanf("%d", &rows);
- 
-  for(i=1; i<=rows; i++)
-  {
-    for(j=i; j<=rows; j++) 
-    {
-      printf("%d ", j);
-    }
-    printf("
+#include<stdio.h>
+int main()
+{
+	int n,v,k;
+	scanf("%d",&n);
+	for(v=n;v>=1;v--)
+	{
+		for(k=1;k<=n;k++)
+		{
+			if(k+v==n+1||k-v==0)
+			{
+				printf("%d ",v);
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("
 ");
-  }
+	}
+	return 0;
 }
